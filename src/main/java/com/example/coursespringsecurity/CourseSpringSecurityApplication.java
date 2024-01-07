e1fa73858ccd9e67e54d4770e822c94ae8b0e1a7
tomszociepa <tom.ociepa@gmail.com>
2024-01-07 10:41:10 +0100

    initial project

---
+++ src/main/java/com/example/coursespringsecurity/CourseSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursespringsecurity/CourseSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
