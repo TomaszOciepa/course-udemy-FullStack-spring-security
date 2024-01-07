bb4675c0166bd331e8cc28b2a1506514898785b9
tomszociepa <tom.ociepa@gmail.com>
2024-01-07 13:50:33 +0100

    L366 utworzenie endpointow

--- src/main/java/com/example/coursespringsecurity/Controller.java
+++ src/main/java/com/example/coursespringsecurity/Controller.java
@@ -1,18 +1,62 @@
 package com.example.coursespringsecurity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class Controller {
 
-    @GetMapping("/login")
-    public String login(){
-        return "Zalogowano";
+    private final UserEntityRepository entityRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserService createUser;
+
+    @GetMapping("/hello")
+    public String welcome() {
+        return "This endpoint is not secured";
     }
 
-    @GetMapping("/logout")
-    public String logout(){
-        return "Wylogowano";
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserEntity userInfo) {
+        createUser.createUser(userInfo);
+        return "User created";
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<UserEntity> getAllTheProducts() {
+        return entityRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public UserEntity getProductById(@PathVariable long id) {
+        return entityRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        try{
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            if (authentication.isAuthenticated()) {
+                return jwtService.generateToken(authRequest.getUsername());
+            } else {
+                throw new UsernameNotFoundException("invalid user request !");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
+
+
+
