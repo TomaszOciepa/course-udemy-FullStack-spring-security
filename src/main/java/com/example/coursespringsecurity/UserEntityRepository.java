00b6163a37cbe713c2c53ecc770d1ae552eb5a27
tomszociepa <tom.ociepa@gmail.com>
2024-01-07 13:42:35 +0100

    L365 User Service i Auth Request

--- src/main/java/com/example/coursespringsecurity/UserEntityRepository.java
+++ src/main/java/com/example/coursespringsecurity/UserEntityRepository.java
@@ -1,13 +1,12 @@
 package com.example.coursespringsecurity;
 
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserEntityRepository extends JpaRepository<User, Long> {
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByMail(String mail);
 }
